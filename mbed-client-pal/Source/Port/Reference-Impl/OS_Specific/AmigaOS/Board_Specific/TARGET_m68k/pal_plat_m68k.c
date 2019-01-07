59a4f832564763a75d908d797ec2ddfc74e70148
Juho Eskeli <juho.eskeli@gmail.com>
2019-01-07 13:25:16 -0800

    add include

--- mbed-client-pal/Source/Port/Reference-Impl/OS_Specific/AmigaOS/Board_Specific/TARGET_m68k/pal_plat_m68k.c
+++ mbed-client-pal/Source/Port/Reference-Impl/OS_Specific/AmigaOS/Board_Specific/TARGET_m68k/pal_plat_m68k.c
@@ -14,6 +14,7 @@
  * limitations under the License.
  *******************************************************************************/
 #include "pal_plat_rtos.h"
+#include <sys/types.h>
 #include <sys/mman.h>
 #include <unistd.h>
 #include <fcntl.h>
