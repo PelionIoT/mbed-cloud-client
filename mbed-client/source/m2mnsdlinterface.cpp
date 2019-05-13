2492380739165caadec372c88e0919e6245aee52
Teemu Takaluoma <teemu.takaluoma@arm.com>
2019-05-13 09:43:50 +0300

    mbed-cloud-client 3.1.1

--- mbed-client/source/m2mnsdlinterface.cpp
+++ mbed-client/source/m2mnsdlinterface.cpp
@@ -82,7 +82,7 @@
 #define TRACE_GROUP "mClt"
 #define MAX_QUERY_COUNT 10
 
-const char *MCC_VERSION = "mccv=3.1.0";
+const char *MCC_VERSION = "mccv=3.1.1";
 
 int8_t M2MNsdlInterface::_tasklet_id = -1;
 
